e5bef0cba2b4b85f89a3fe951a9250366f44e05f
vaddv <vijay.vaddem@gmail.com>
2020-06-15 09:38:34 +0530

    Implemented login screen with email, password and login button

--- App.js
+++ App.js
@@ -1,28 +1,7 @@
-import React, { useState } from "react";
-import { TextInput, Text, Switch } from "react-native";
+import React from "react";
 
-import Screen from "./app/components/Screen";
-import AppTextInput from "./app/components/AppTextInput";
-import AppText from "./app/components/AppText.android";
-import AppPicker from "./app/components/AppPicker";
 import LoginScreen from "./app/screens/LoginScreen";
 
-const categories = [
-  {
-    label: "Furniture",
-    value: 1,
-  },
-  {
-    label: "Clothing",
-    value: 2,
-  },
-  {
-    label: "Cameras",
-    value: 3,
-  },
-];
-
 export default function App() {
-  const [selectedCategory, setSelectedCategory] = useState(categories[0]);
   return <LoginScreen />;
 }
