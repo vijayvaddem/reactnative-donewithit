7abf001f75a6ed41970d49fd643b07a85320756b
vaddv <vijay.vaddem@gmail.com>
2020-06-21 14:00:12 +0530

    Added new and reusable ImagePicker component

--- App.js
+++ App.js
@@ -3,39 +3,17 @@ import React, { useEffect, useState } from "react";
 import Screen from "./app/components/Screen";
 import * as ImagePicker from "expo-image-picker";
 import { Button, Image } from "react-native";
+import ImageInput from "./app/components/ImageInput";
 
 export default function App() {
   const [imageUri, setImageUri] = useState();
 
-  const requestPermission = async () => {
-    const { granted } = await ImagePicker.requestCameraRollPermissionsAsync();
-    if (!granted) alert("You need to enable access to the library");
-  };
-
-  useEffect(() => {
-    requestPermission();
-  }, []);
-
-  const selectImage = async () => {
-    try {
-      const result = await ImagePicker.launchImageLibraryAsync();
-      if (!result.cancelled) setImageUri(result.uri);
-    } catch (error) {
-      console.log("Error reading an image", error);
-    }
-  };
-
   return (
     <Screen>
-      <Button title="Select Image" onPress={selectImage}></Button>
-      <Image
-        source={{ uri: imageUri }}
-        style={{
-          width: 200,
-          height: 200,
-          borderWidth: 2,
-        }}
-      ></Image>
+      <ImageInput
+        imageUri={imageUri}
+        onChangeImage={(uri) => setImageUri(uri)}
+      />
     </Screen>
   );
 }
