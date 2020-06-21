c4abacaa54ad282a7637b3fcef1f2138fcfbe2e6
vaddv <vijay.vaddem@gmail.com>
2020-06-21 09:03:56 +0530

    Trying with ImagePicker along with permissions

--- App.js
+++ App.js
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-import RegisterScreen from "./app/screens/RegisterScreen";
-import ListingEditScreen from "./app/screens/ListingEditScreen";
-import MessagesScreen from "./app/screens/MessagesScreen";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
+import Screen from "./app/components/Screen";
+import * as ImagePicker from "expo-image-picker";
+import { Button, Image } from "react-native";
 
 export default function App() {
-  return <RegisterScreen />;
+  const [imageUri, setImageUri] = useState();
+
+  const requestPermission = async () => {
+    const { granted } = await ImagePicker.requestCameraRollPermissionsAsync();
+    if (!granted) alert("You need to enable access to the library");
+  };
+
+  useEffect(() => {
+    requestPermission();
+  }, []);
+
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync();
+      if (!result.cancelled) setImageUri(result.uri);
+    } catch (error) {
+      console.log("Error reading an image", error);
+    }
+  };
+
+  return (
+    <Screen>
+      <Button title="Select Image" onPress={selectImage}></Button>
+      <Image
+        source={{ uri: imageUri }}
+        style={{
+          width: 200,
+          height: 200,
+          borderWidth: 2,
+        }}
+      ></Image>
+    </Screen>
+  );
 }
