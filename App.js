5ffd00963db1efed0a1c28ca4749706c65f01aff
vaddv <vijay.vaddem@gmail.com>
2020-06-22 09:52:12 +0530

    started off with Stack Navigation

--- App.js
+++ App.js
@@ -1,23 +1,49 @@
 import React, { useEffect, useState } from "react";
+import { Button } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
 
 import Screen from "./app/components/Screen";
 import ImageInputList from "./app/components/ImageInputList";
 import ListingEditScreen from "./app/screens/ListingEditScreen";
+import AppText from "./app/components/Text";
 
-export default function App() {
-  const [imageUris, setImageUris] = useState([]);
-
-  const handleAdd = (uri) => {
-    setImageUris([...imageUris, uri]);
-  };
-
-  const handleDelete = (uri) => {
-    setImageUris(imageUris.filter((u) => u !== uri));
-  };
-
+function Tweets({ navigation }) {
   return (
     <Screen>
-      <ListingEditScreen />
+      <AppText>Tweets</AppText>
+      <Button
+        title="Go to Tweet details"
+        onPress={() => navigation.navigate("Tweet details")}
+      />
     </Screen>
+  );
+}
+
+function TweetDetails({ navigation }) {
+  return (
+    <Screen>
+      <AppText>Tweet details</AppText>
+      <Button title="Go back" onPress={() => navigation.navigate("Tweets")} />
+    </Screen>
+  );
+}
+
+const Stack = createStackNavigator();
+
+function StackNavigator() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Tweets" component={Tweets} />
+      <Stack.Screen name="Tweet details" component={TweetDetails} />
+    </Stack.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
   );
 }
