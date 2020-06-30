d0c061984da4977e989198bb78d58fed9089a80e
vaddv <vijay.vaddem@gmail.com>
2020-06-30 23:26:56 +0530

    Implemented resetting of form once the data is submitted

--- app/components/forms/FormField.js
+++ app/components/forms/FormField.js
@@ -5,13 +5,21 @@ import TextInput from "../TextInput";
 import ErrorMessage from "./ErrorMessage";
 
 function AppFormField({ name, width, ...otherProps }) {
-  const { setFieldTouched, handleChange, errors, touched } = useFormikContext();
+  const {
+    setFieldTouched,
+    setFieldValue,
+    handleChange,
+    errors,
+    touched,
+    values,
+  } = useFormikContext();
 
   return (
     <>
       <TextInput
         onBlur={() => setFieldTouched(name)}
-        onChangeText={handleChange(name)}
+        onChangeText={(text) => setFieldValue(name, text)}
+        value={values[name]}
         width={width}
         {...otherProps}
       />
--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -44,7 +44,7 @@ function ListingEditScreen(props) {
   const [uploadVisible, setUploadVisible] = useState(false);
   const [progress, setProgress] = useState(0);
 
-  const handleSubmit = async (listing) => {
+  const handleSubmit = async (listing, { resetForm }) => {
     setProgress(0);
     setUploadVisible(true);
     const result = await listingsApi.addListing(
@@ -56,6 +56,8 @@ function ListingEditScreen(props) {
       setUploadVisible(false);
       return alert("Could not save the listing");
     }
+
+    resetForm();
     // alert("Success");
   };
 
