3506bd6825724dc40579ae8095c0b09b926425c0
vaddv <vijay.vaddem@gmail.com>
2020-07-11 16:14:06 +0530

    Added indicator activity support during registration process

--- app/screens/RegisterScreen.js
+++ app/screens/RegisterScreen.js
@@ -3,10 +3,17 @@ import { StyleSheet } from "react-native";
 import * as Yup from "yup";
 
 import Screen from "../components/Screen";
-import { Form, FormField, SubmitButton } from "../components/forms";
+import {
+  Form,
+  FormField,
+  SubmitButton,
+  ErrorMessage,
+} from "../components/forms";
 import usersApi from "../api/users";
 import useAuth from "../auth/useAuth";
 import authApi from "../api/auth";
+import useApi from "../hooks/useApi";
+import ActivityIndicator from "../components/ActivityIndicator";
 
 const validationSchema = Yup.object().shape({
   name: Yup.string().required().label("Name"),
@@ -15,12 +22,14 @@ const validationSchema = Yup.object().shape({
 });
 
 function RegisterScreen(props) {
+  const registerApi = useApi(usersApi.register);
   const auth = useAuth();
+  const loginApi = useApi(authApi.login);
+
   const [error, setError] = useState();
 
   const handleSubmit = async (userInfo) => {
-    const result = await usersApi.register(userInfo);
-
+    const result = await registerApi.request(userInfo);
     if (!result.ok) {
       if (result.data) setError(result.data.error);
       else {
@@ -30,7 +39,7 @@ function RegisterScreen(props) {
       return;
     }
 
-    const { data: authToken } = await authApi.login(
+    const { data: authToken } = await loginApi.request(
       userInfo.email,
       userInfo.password
     );
@@ -38,11 +47,13 @@ function RegisterScreen(props) {
   };
   return (
     <Screen style={styles.container}>
+      <ActivityIndicator visible={registerApi.loading || loginApi.loading} />
       <Form
         initialValues={{ name: "", email: "", password: "" }}
         validationSchema={validationSchema}
         onSubmit={handleSubmit}
       >
+        <ErrorMessage error={error} visible={error} />
         <FormField
           autoCapitalize="none"
           autoCorrect={false}
